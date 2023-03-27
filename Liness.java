5eba46df36a4c84258fe448dc6270f134afab3df
Shivam mahajan <124094950+mahajan1806@users.noreply.github.com>
2023-03-28 03:23:34 +0530

    Update Liness.java

--- Liness.java
+++ Liness.java
@@ -1,8 +1,4 @@
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
 package liness;
 
 import java.util.ArrayList;

7104f055929137938401cfdae1888df9eac0e6f0
lament29 <51352257+lament29@users.noreply.github.com>
2023-05-04 17:32:34 -0500

    Added the missing variables. Looks good now.

--- src/initialSetup/WallsStoring.java
+++ src/initialSetup/WallsStoring.java
@@ -11,4 +11,25 @@ public class WallsStoring {
         this.y = y;
         this.z = z;
         this.length = length;
+
+    public Wall createWalls(){
+        Wall rect = new wall(x, y, z, length);
+        return rect;
+        }
+
+    public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    ArrayList<Wall> walls = new ArrayList<>();
+
+    // Add horizontal walls
+    walls.add(new Wall(50, 50, 1,100));
+    walls.add(new Wall(200, 50, 1, 150));
+    walls.add(new Wall(400, 150, 1, 100));
+
+    // Add vertical walls
+    walls.add(new Wall(50, 50, 2, 100));
+    walls.add(new Wall(50, 200, 2, 150));
+    walls.add(new Wall(250, 100, 2, 50));
+    }
 }
